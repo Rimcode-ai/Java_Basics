32afd896dbf629d0d5520981420b52dfa10b6af8
Rima Modak <sas999modak111@gmail.com>
2024-08-14 00:29:18 +0000

    update

--- Loops/continueBySkipping3.java
+++ Loops/continueBySkipping3.java
@@ -1,11 +1,11 @@
-public class breakWhen3IsFound {
+public class continueBySkipping3 {
     public static void main(String[] args) {
         Integer [] arraySample = {1,2,3,4,5,6,7,8,9};
         for (int i=0; i < arraySample.length; i++){
             System.out.print(arraySample[i]+" : ");
             for (int j = 0; j < arraySample.length; j++){
                 if (arraySample[i] == 3 && arraySample[j] == 3){
-                    break;
+                    continue;
                 }
                 System.out.print(arraySample[j] + "  ");
             }
